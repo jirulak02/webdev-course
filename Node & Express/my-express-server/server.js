33dd7a405a8348bd8ce4f7e7a51b723327746569
jirulak02 <tchooorulok@gmail.com>
2023-03-09 15:56:57 +0100

    day 6

--- Node & Express/my-express-server/server.js
+++ Node & Express/my-express-server/server.js
@@ -16,7 +16,7 @@ app.get("/about", function(req, res) {
 
 app.listen(3000, (error) => {
 	if(!error)
-		console.log("Server is Successfully Running, and App is listening on port 3000")
+		console.log("Server is Successfully Running, and App is listening on port 3000.")
 	else {
 		console.log("Error occurred, server can't start", error);
 	}
