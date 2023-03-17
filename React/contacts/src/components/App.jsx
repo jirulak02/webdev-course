025682640dca34dd1851e4f8a0709fa9ec362e9c
jirulak02 <tchooorulok@gmail.com>
2023-03-17 22:14:12 +0100

    day 14

---
+++ React/contacts/src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import contacts from "../contacts";
+import Card from "./Card";
+
+function App() {
+	return (
+		<div>
+		<h1 className="heading">My Contacts</h1>
+		{contacts.map((contact) => (
+			<Card
+			key={contact.id}
+			name={contact.name}
+			imgURL={contact.imgURL}
+			phone={contact.phone}
+			email={contact.email}
+			/>
+		))}
+		</div>
+	);
+}
+
+export default App;
