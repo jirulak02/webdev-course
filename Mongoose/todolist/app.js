55c7c5f477553e9f476d4c85278203e4929a40dd
jirulak02 <tchooorulok@gmail.com>
2023-04-11 18:14:11 +0200

    day 11

--- Mongoose/todolist/app.js
+++ Mongoose/todolist/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -5,60 +6,59 @@ const _ = require("lodash");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://user:test-123@cluster0.rq2usmb.mongodb.net/todolistDB", {
+mongoose.connect(process.env.DB_URL, {
 	useNewUrlParser: true,
-	useUnifiedTopology: true
+	useUnifiedTopology: true,
 });
 
 const itemsSchema = new mongoose.Schema({
-	name: String
+	name: String,
 });
 const Item = mongoose.model("Item", itemsSchema);
 
 const listSchema = new mongoose.Schema({
 	name: String,
-	items: [itemsSchema]
+	items: [itemsSchema],
 });
 const List = mongoose.model("List", listSchema);
 
 const item1 = new Item({
-	name: "Clean my room"
+	name: "Clean my room",
 });
 
 const item2 = new Item({
-	name: "Finish the WebDev course"
+	name: "Finish the WebDev course",
 });
 
 const item3 = new Item({
-	name: "Start React course"
+	name: "Start React course",
 });
 
 const defaultItems = [item1, item2, item3];
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
 	async function myFindAll() {
 		const findItems = await Item.find({});
 		if (findItems.length === 0) {
 			await Item.insertMany(defaultItems);
 		}
-		res.render("list", {listTitle: "Today", newListItems: findItems});
+		res.render("list", { listTitle: "Today", newListItems: findItems });
 	}
 	// still no idea how to make the disconnect work with awaits :)
-	myFindAll()/*.finally(() => mongoose.disconnect())*/;
+	myFindAll() /*.finally(() => mongoose.disconnect())*/;
 });
 
-app.post("/", function(req, res){
-
+app.post("/", function (req, res) {
 	const itemName = req.body.newItem;
 	const listName = req.body.list;
 
 	const newItem = new Item({
-		name: itemName
+		name: itemName,
 	});
 
 	if (listName === "Today") {
@@ -66,7 +66,7 @@ app.post("/", function(req, res){
 		res.redirect("/");
 	} else {
 		async function myFindList() {
-			const foundList = await List.findOne({name: listName});
+			const foundList = await List.findOne({ name: listName });
 
 			foundList.items.push(newItem);
 			foundList.save();
@@ -76,7 +76,7 @@ app.post("/", function(req, res){
 	}
 });
 
-app.post("/delete", function(req, res) {
+app.post("/delete", function (req, res) {
 	async function deleteItem() {
 		const checkedItemId = req.body.checkbox;
 		const listName = req.body.listName;
@@ -86,9 +86,12 @@ app.post("/delete", function(req, res) {
 
 			res.redirect("/");
 		} else {
-			await List.findOneAndUpdate({name: listName}, {
-				$pull: {items: {_id: checkedItemId}}
-			});
+			await List.findOneAndUpdate(
+				{ name: listName },
+				{
+					$pull: { items: { _id: checkedItemId } },
+				}
+			);
 
 			res.redirect("/" + listName);
 		}
@@ -96,21 +99,24 @@ app.post("/delete", function(req, res) {
 	deleteItem();
 });
 
-app.get("/:paramName", function(req,res){
+app.get("/:paramName", function (req, res) {
 	async function myFindOne() {
 		const enteredName = _.capitalize(req.params.paramName);
 
-		const findList = await List.findOne({name: enteredName});
+		const findList = await List.findOne({ name: enteredName });
 		if (!findList) {
 			const list = new List({
 				name: enteredName,
-				items: defaultItems
+				items: defaultItems,
 			});
 
 			list.save();
 			res.redirect("/" + enteredName);
 		} else {
-			res.render("list", {listTitle: enteredName, newListItems: findList.items});
+			res.render("list", {
+				listTitle: enteredName,
+				newListItems: findList.items,
+			});
 		}
 	}
 	myFindOne();
@@ -121,6 +127,6 @@ if (port == null || port == "") {
 	port = 3000;
 }
 
-app.listen(port, function() {
+app.listen(port, function () {
 	console.log("Server started successfully");
 });
